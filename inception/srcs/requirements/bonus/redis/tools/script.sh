856fb75135ce90d7c5ec69cf49a5bdfbf19c6779
Abdlatif-20 <rca8272@gmail.com>
2024-07-19 15:58:12 +0100

    some update

--- inception/srcs/requirements/bonus/redis/tools/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Configure Redis
-# Comment out the bind address to allow connections from any IP
-sed -i "s/bind 127.0.0.1/#bind 127.0.0.1/" /etc/redis/redis.conf
-
-# Start Redis server in the foreground with the updated configuration
-redis-server --protected-mode no
