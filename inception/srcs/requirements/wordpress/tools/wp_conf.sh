c89bbf3998fc11b29982068523b171aa0a4ea6ba
Abdlatif-20 <rca8272@gmail.com>
2024-07-23 00:29:27 +0100

    finish_incep

--- inception/srcs/requirements/wordpress/tools/wp_conf.sh
+++ inception/srcs/requirements/wordpress/tools/wp_conf.sh
@@ -2,10 +2,13 @@
 
 #---------------------------------------------------wp installation---------------------------------------------------#
 sleep 10
+
+mkdir -p /run/php
+sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" /etc/php/7.4/fpm/pool.d/www.conf
 cd /var/www/html;
 #---------------------------------------------------wp installation---------------------------------------------------##---------------------------------------------------wp installation---------------------------------------------------#
 wp core download --allow-root
-wp config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --allow-root
+mv wp-config-sample.php wp-config.php
 wp config set DB_HOST mariadb:3306 --allow-root
 wp config set DB_NAME $MYSQL_DB --allow-root
 wp config set DB_USER $MYSQL_USER --allow-root
@@ -16,7 +19,4 @@ wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_N --admin
 #create a new user with the given username, email, password and role
 wp user create $WP_U_NAME $WP_U_EMAIL --user_pass=$WP_U_PASS --role=$WP_U_ROLE --allow-root
 
-#---------------------------------------------------php config---------------------------------------------------#
-sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
-mkdir -p /run/php
 /usr/sbin/php-fpm7.4 -F
